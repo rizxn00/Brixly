52c6657f2ed2c7ae1b69203efc0b0059025f9394
rizxn00 <rizi.n2021@gmail.com>
2025-07-09 00:34:25 +0530

    fix: update import statements to include file extensions for clarity

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home'
-import ProductDetailPage from './pages/ProductDetailPage'
-import SearchResultsPage from './pages/SearchResultsPage'
-import DealersList from './pages/DealersList'
+import HomePage from './pages/Home.tsx'
+import ProductDetailPage from './pages/ProductDetailPage.tsx'
+import SearchResultsPage from './pages/SearchResultsPage.tsx'
+import DealersList from './pages/DealersList.tsx'
 
 function App() {
   return (
